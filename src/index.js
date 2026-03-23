c06ad7b292aef20a9e23d9cddcc6d76506dd6256
bhupenderhere <bhupender.contact@gmail.com>
2026-03-23 14:39:51 +0530

    temp

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+import "./index.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import App from "./routes";
 import "leaflet/dist/leaflet.css";
-import App from "./app/App";
 import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 import store, { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-    </React.StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
